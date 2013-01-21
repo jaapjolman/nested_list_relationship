53bd274a1cd79a47f50ef5052fe2ca298183772c
Jacob Albert Jolman <jaap@jolman.eu>
2013-01-21 20:04:15 +0100

    Dutch Language Added

---
+++ language/dutch/nested_list_relationship_lang.php
@@ -0,0 +1,7 @@
+<?php defined('BASEPATH') or exit('No direct script access allowed');
+
+$lang['streams.nested_list_relationship.name'] 						= 'Hoofd Detail Relatie';
+$lang['streams.nested_list_relationship.master_detail_stream'] 		= 'Hoofd Detail Stream';
+$lang['streams.nested_list_relationship.allow_disabled_status']		= 'Uitgeschakelde Status Toestaan';
+$lang['streams.nested_list_relationship.doesnt_exist']				= 'Gerelateerde stream bestaat niet.';
+$lang['streams.nested_list_relationship.empty']						= 'Gerelateerde stream is leeg.';
